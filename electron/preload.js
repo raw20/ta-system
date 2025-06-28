f615dde0883a5aac6e7d45fb1de3bef26ccc46c7
정찬욱 <raw1250@gmail.com>
2025-06-28 20:58:30 +0900

    fix: preload 초기 세팅 재설정

--- electron/preload.js
+++ electron/preload.js
@@ -1,7 +1,11 @@
-const { contextBridge, ipcRenderer } = require("electron");
+import { contextBridge, ipcRenderer } from "electron";
 
 contextBridge.exposeInMainWorld("electronAPI", {
-  getEmployees: () => ipcRenderer.invoke("db:get-employees"),
+  // 🧪 테스트용 API
+  testDbConnection: () => ipcRenderer.invoke("test:db-connection"),
+  getEmployees: () => ipcRenderer.invoke("test:get-employees"),
+
+  // 향후 추가될 API들
   createEmployee: (data) => ipcRenderer.invoke("db:create-employee", data),
   exportExcel: (type, data) => ipcRenderer.invoke("export:excel", type, data),
   selectFolder: () => ipcRenderer.invoke("dialog:select-folder"),
