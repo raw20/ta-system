c399a23e1036f739b56ba60be181658c7114318b
정찬욱 <raw1250@gmail.com>
2025-07-10 19:14:52 +0900

    feat: 기초 레이아웃 설정

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
 
-export default function Layout() {
-  return <div>Layout</div>;
-}
+const Layout = () => {
+  return (
+    <div className="flex h-screen bg-gray-100">
+      {/* 사이드바 */}
+      <Sidebar />
+
+      {/* 메인 컨텐츠 영역 */}
+      <div className="flex-1 flex flex-col overflow-hidden">
+        {/* 헤더 */}
+        <Header />
+
+        {/* 메인 컨텐츠 */}
+        <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-50 p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
