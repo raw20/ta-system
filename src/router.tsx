c89886888cb71d5800a397e9d191e4e9d27d3f2b
정찬욱 <raw1250@gmail.com>
2025-08-05 18:52:02 +0900

    feat: 사용자 상세정보 추가

--- src/router.tsx
+++ src/router.tsx
@@ -29,7 +29,7 @@ export const router = createBrowserRouter([
         element: <EmployeeAdd />,
       },
       {
-        path: "/employees/edit",
+        path: "/employees/edit/:id",
         element: <EmployeeEdit />,
       },
       {
